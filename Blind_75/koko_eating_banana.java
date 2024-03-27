83494a015afca7ca0397e62e77469e08f8ad2221
Yash poniya <96628229+yashponiya001@users.noreply.github.com>
2024-03-27 23:25:22 +0530

    Update koko_eating_banana.java

--- Blind_75/koko_eating_banana.java
+++ Blind_75/koko_eating_banana.java
@@ -26,4 +26,45 @@ class Solution {
             return true;
         return false;
     }
+} 
+//with comments
+/*class Solution {
+    // Method to find the minimum eating speed
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1; // The minimum possible eating speed
+        int high = Integer.MIN_VALUE; // The maximum possible eating speed
+        
+        // Find the maximum pile size to set as the initial high value
+        for(int i = 0; i < piles.length; i++){
+            high = Math.max(high, piles[i]);
+        }
+        
+        // Binary search to find the minimum eating speed
+        while(low < high){
+            int mid = low + (high - low) / 2; // Midpoint of low and high
+            // If it's possible to eat all bananas at this speed within h hours
+            if(blackbox(mid, piles, h)){
+                high = mid; // Try a lower speed
+            }
+            else {
+                low = mid + 1; // Try a higher speed
+            }
+        }
+        // The lowest possible eating speed that allows finishing within h hours
+        return low;
+    }
+    
+    // Helper method to check if it's possible to eat all bananas at a given speed within h hours
+    public boolean blackbox(int maxpiles, int[] piles, int h){
+        int hours = 0; // Total hours needed to eat all piles at the given speed
+        for(int i : piles){
+            int time = i / maxpiles; // Hours needed to eat the current pile
+            hours += time;
+            // If there are leftover bananas, add an extra hour
+            if(i % maxpiles != 0) hours++;
+        }
+        // Return true if the total hours is less than or equal to h
+        return hours <= h;
+    }
 }
+*/

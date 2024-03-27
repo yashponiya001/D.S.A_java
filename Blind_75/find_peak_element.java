2fdcdad789ce59456dc6cba061fccf1a670e54e1
Yash poniya <96628229+yashponiya001@users.noreply.github.com>
2024-03-27 23:20:24 +0530

    Create find_peak_element.java

---
+++ Blind_75/find_peak_element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+      int n=nums.length;
+        int[] arr;
+        if(n==1){
+            return 0;
+        }
+        else if (nums[0]>nums[1]){
+            return 0;
+        }
+        else if(nums[n-1]>nums[n-2])
+        {
+            return n-1;
+        }
+        else
+        {
+            for(int i=1;i<n-1;i++){
+                if(nums[i-1]<=nums[i] && nums[i]>=nums[i+1]){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
